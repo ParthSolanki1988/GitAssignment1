dcb4ed06c9b3eb9525975f152d6a376d03b89702
SimformParth <parthsolanki.sp1812@gmail.com>
2023-02-20 08:52:40 +0530

    modified for loop to while loop in fibo file in feature branch

--- Fibo.java
+++ Fibo.java
@@ -1,16 +1,18 @@
 class Fibo {
+ //display fibo using while loop
   public static void main(String[] args) {
 
-    int n = 10, firstTerm = 0, secondTerm = 1;
+    int i = 1, n = 10, firstTerm = 0, secondTerm = 1;
     System.out.println("Fibonacci Series till " + n + " terms:");
 
-    for (int i = 1; i <= n; ++i) {
+    while (i <= n) {
       System.out.print(firstTerm + ", ");
 
-      // compute the next term
       int nextTerm = firstTerm + secondTerm;
       firstTerm = secondTerm;
       secondTerm = nextTerm;
+
+      i++;
     }
   }
 }
